ff65ca5f66ea179b6ff2364839ebfa1737a13c98
Hardeep Singh Tiwana <36938993+hstiwana@users.noreply.github.com>
2020-06-16 01:00:00 -0700

    script with for loop on a list

---
+++ for_loop_list.py
@@ -0,0 +1,7 @@
+# a simple for loop to calculate total from a list
+# e.g. if we need to total expense over a couple of months 
+expense=[1000,5000,2350,5500,3400,4500]
+total=0
+for expX in expense:
+    total = total + expX
+print(total)
