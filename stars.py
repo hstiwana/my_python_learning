00b1101308d5769fa16438214e044f85a9626a05
Hardeep Singh Tiwana <36938993+hstiwana@users.noreply.github.com>
2020-06-19 00:29:06 -0700

    3 ways and loops to show difference

--- stars.py
+++ stars.py
@@ -1,21 +1,29 @@
 # Program printing starts as shown below
-# *
-# **
-# ***
-# ****
-# *****
-# ******
-#### this also works
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * * * 
+
+#### this works
 # s='*'
 # print(s) # print first star
 # for i in range(1,6): #lets make a list of 6 stars
-#     k='*'
+#     k='* '
 #     for j in range(i): #go one item at a time and add stars, it will start with 2 stars
-#         k+='*'
+#         k+='* '
 #     print(k)
+
 #### this works better :) 
-for i in range(1,6): 
-    s = ''
-    for j in range(i): 
-        s += '*'
-    print(s)
+# for i in range(1,6): 
+#     s = ''
+#     for j in range(i): 
+#         s += '* '
+#     print(s)
+
+### see it look even better with less lines and few vars 
+for i in range(1,7):
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print()#to give line break after each iteration 
