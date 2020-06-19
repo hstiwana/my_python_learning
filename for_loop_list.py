9c8857e9d48005165314db558108422ebc50411f
Hardeep Singh Tiwana <36938993+hstiwana@users.noreply.github.com>
2020-06-18 22:06:04 -0700

    for loop with some calculations on the fly and show total in the end

--- for_loop_list.py
+++ for_loop_list.py
@@ -1,7 +1,12 @@
 # a simple for loop to calculate total from a list
 # e.g. if we need to total expense over a couple of months 
-expense=[1000,5000,2350,5500,3400,4500]
+expense=[1000,5000,2350,5500,3400,4500,2300,2500,3789,3421,2312,3300]
 total=0
-for expX in expense:
-    total = total + expX
-print(total)
+# for expX in expense:
+#     total = total + expX
+# print(total)
+for exp2 in range(len(expense)):
+    print(f'Month: {exp2+1}, Expense {expense[exp2]}')
+    # print('Month:',exp2+1,'Expense',expense[exp2])
+    total = total + expense[exp2]
+print('Total Yearly Expense is:',total)
