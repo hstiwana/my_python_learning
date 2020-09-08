a8e44f8706b8b42638e8371310f2d031d4332188
Hardeep Singh Tiwana <36938993+hstiwana@users.noreply.github.com>
2020-09-07 19:19:01 -0700

    [HT] removed a few unwanted lines from devops_questions.py

--- devops_question.py
+++ devops_question.py
@@ -6,35 +6,27 @@ user_resp = [ 'yes', 'no']
 tech = [ 'yes']
 print( "Lets see if you are eligible for Dev Ops role: ")
 try:
-    skill1 = ''
-    while True: # loop to wait for right input from user 
-        if skill1 == '':
-            skill1 = input(f'What coding language you know, Options {language}: ').lower()
-        elif skill1 not in language:
+    skill1 = input(f'What coding language you know, Options {language}: ').lower()
+    while True: # loop to wait for right input from user
+        if skill1 not in language:
             skill1 = input(f'What coding language you know, Options {language}: ').lower()
         else:
             break # lets get out of this loop if user input is one of the language
-    skill2 = ''
+    skill2 = input('Do you know K8s? (yes/no): ').lower()
     while True: # loop to wait for correct input
-        if skill2 == '':
-            skill2 = input('Do you know K8s? (yes/no): ').lower()
-        elif skill2 not in user_resp:
+        if skill2 not in user_resp:
             skill2 = input('Do you know K8s? (yes/no): ').lower()
         else:
             break # lets get out of this loop if user input is one of the user_resp
-    skill3 = ''
+    skill3 = input('Do you know Azure? (yes/no): ').lower()
     while True: # loop to wait for correct input
-        if skill3 == '':
-            skill3 = input('Do you know Azure? (yes/no): ').lower()
-        elif skill3 not in user_resp:
+        if skill3 not in user_resp:
             skill3 = input('Do you know Azure? (yes/no): ').lower()
         else:
             break # lets get out of this loop if user input is one of the user_resp
-    skill4 = ''
+    skill4 = input('Number of years you have worked in cloud? : ')
     while True: # loop to wait for correct input
-        if skill4 == '':
-            skill4 = input('Number of years you have worked in cloud? : ')
-        elif skill4.isdigit() == False:
+        if skill4.isdigit() == False:
             print('Invalid input: Please enter numbers only!')
             skill4 = input('Number of years you have worked in cloud? : ')
         else:
@@ -47,4 +39,5 @@ try:
     elif skill2 == 'no' or skill3 == 'no':
         print('Sorry, you are lacking expertise in some of the required skills e.g. Azure or K8s')
 except KeyboardInterrupt:
+    print('\n\nExiting out on keyboard interrupt')
     sys.exit()
